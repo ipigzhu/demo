5476c7a53f72179d3bcb5a4a5fea9b04222496dc
ipigzhu <ipigzhu@hotmail.com>
2018-09-29 17:57:38 +0800

    999

--- project/forms/__init__.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-#coding=utf-8
-__author__ = "ipigzhu"
-
-
-from .user_fm import UserRegForm
-
--- project/models/__init__.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-#coding=utf-8
-__author__ = "ipigzhu"
-
-
-from .index_md import User
-
